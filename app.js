a2bea4513360345761ec1f18e6b203dfee3f1af0
yu-zhengyu <yuzheng@andrew.cmu.edu>
2016-01-08 22:57:14 -0500

    add database

--- app.js
+++ app.js
@@ -2,16 +2,44 @@ var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server),
+    mongoose = require ('mongoose'),
     nicknameArray = [];
 
-server.listen(3000);
+server.listen(3000); // listen the 3000 port, and start the server.
+
+// connect our mongodb database;  
+mongoose.connect('mongodb://localhost/nodechat', function(err) {
+    if(err) {
+        console.log(err);
+    }
+    else {
+        console.log('Mongodb connect successful'); 
+    }
+}); 
+
+var chatschema = mongoose.Schema({
+    nick: String,
+    msg: String
+});
+
+var Chat = mongoose.model('Message', chatschema);
 
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
+app.use(express.static('public'));
+
 io.sockets.on('connection', function(socket){
+    Chat.find({}, function(err, docs) {
+        if(err){
+            throw err;
+        }
+        socket.emit('load message', docs); 
+
+    });
+
     socket.on('new user', function(data, callback){
         if(nicknameArray.indexOf(data) != -1) {
             callback(false);
@@ -30,6 +58,11 @@ io.sockets.on('connection', function(socket){
     });
     
     socket.on('send message', function(data){
-       io.sockets.emit('new message', {msg:data, nick:socket.nickname}); // send message for every one include me
+        var newMessage = new Chat({nick:socket.nickname, msg:data});
+        newMessage.save(function(err) {
+            if(err) throw err;
+            io.sockets.emit('new message', {msg:data, nick:socket.nickname}); // send message for every one include me
+        });
+        
     });
 });
---
+++ public/basic.js
@@ -0,0 +1,46 @@
+jQuery(function($){
+   var socket = io.connect();
+   var $messageForm = $('#send-message');
+   var $messageBox = $('#message');
+   var $chat = $('#chat');
+   var $nicknameForm = $('#setNick');
+   var $nicknameError = $('#nickNameError');
+   var $nicknameBox = $('#nickname');
+   
+   $messageForm.submit(function(e){
+      e.preventDefault();
+      socket.emit('send message', $messageBox.val());
+      $messageBox.val(''); 
+   });
+   
+   $nicknameForm.submit(function(e) {
+      e.preventDefault();
+      socket.emit('new user', $nicknameBox.val(), function(data) { 
+          if(data) {
+              $('#nickName').hide();
+              $('#contentWrap').show();
+          } else {
+              $nicknameError.html("This name is already exist.");
+          }
+      });
+      $nicknameBox.val('');
+   });
+   
+  socket.on('load message', function(docs) {
+    for(var i = 0; i < docs.length; i++) {
+      displaymes(docs[i]);
+    }
+    var div = document.getElementById('chat');
+    div.scrollTop = div.scrollHeight; 
+  });
+
+   socket.on('new message', function(data){
+      displaymes(data);
+   });
+
+   function displaymes(data) {
+    $chat.append(Date().toLocaleString().slice(0, -14) + "<br/>" + data.nick + ": " + data.msg + "<br/>");
+    var div = document.getElementById('chat');
+    div.scrollTop = div.scrollHeight; 
+   }
+});
